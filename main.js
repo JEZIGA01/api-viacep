01e103d6a8d91a7f0e882bf39935c9bd034da65c
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2022-08-12 19:00:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const inputCep = document.getElementById('cep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+inputCep.addEventListener('keyup', showCEP);
+
+function showCEP(event) {
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.json())
+    .then(body => {
+      console.log(body) 
+      resultadoCep.innerText = `
+    O seu logradouro é: ${body.logradouro}, localizado na cidade: ${body.localidade},
+     no bairro : ${body.bairro}, estado : ${body.uf}
+    `;
+    })
+}
